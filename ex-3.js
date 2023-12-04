6c08f401ee7c9f94dff6d4864015ff88c1571d0e
saranchai <saranchai697@gmail.com>
2023-12-05 01:10:33 +0700

    feat: already answered every exercise.

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,14 @@
 //Exercise 3
 // Ternary Operator
 let lightBulbStatus = "Off";
-
+let lightBulb;
 //Start coding here
+
+/* if(lightBulbStatus === "On") {
+    console.log("Light bulb is On.");
+} else {
+    console.log("Light bulb is Off.");
+} */
+
+lightBulbStatus === "Off" ? lightBulb = "Light bulb is Off." : lightBulb = `Please choose the correct input (On/Off)`;
+console.log(lightBulb); 
